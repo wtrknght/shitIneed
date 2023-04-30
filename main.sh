57b1e0575586a6ea4d90a0858944981a9a31bc83
wtrknght <116600901+wtrknght@users.noreply.github.com>
2023-04-30 17:23:48 -0400

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,4 @@
+wget https://flathub.org/repo/flathub.gpg
+cp flathub.gpg ~/.local/share/flatpak/repo/flathub.trustedkeys.gpg
+cp flathub.gpg ~/.local/share/flatpak/repo/flathub-beta.trustedkeys.gpg
+ls -la ~/.local/share/flatpak/repo/*.trustedkeys.gpg

2f11d227adbfbb80ed05bb2bff764691e9fed0dc
wtrknght <116600901+wtrknght@users.noreply.github.com>
2023-07-30 22:21:15 -0400

    Create mainer.sh

---
+++ mainer.sh
@@ -0,0 +1,13 @@
+emerge x11-libs/libX11
+emerge x11-base/xorg-server
+emerge x11-libs/libXrandr
+emerge x11-libs/libXinerama
+emerge x11-libs/libXft
+emerge x11-apps/xinit
+emerge x11-apps/xrdb
+emerge x11-apps/mesa-progs
+emerge x11-apps/xrandr
+emerge x11-drivers/xf86-video-vboxvideo
+emerge x11-misc/unclutter
+emerge x11-misc/xclip
+emerge x11-misc/pcmanfm
